55c751499dff4bd898597719faaee02eb130796e
Pratiksha Bhosale <pratikshabhosale010@gmail.com>
2024-08-24 15:11:28 +0000

    container config code

---
+++ javascript/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "pratiksha@googal.com"
+var accountPassword = "12345"
+accountCity = "Latur"
+
+//accountId = 2 // not
+
+
+
+accountEmail = "pb@pb.com"
+accountPassword = "21212121"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
