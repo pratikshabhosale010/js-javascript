4a744f714b1e52d352101e5a77a5047124268cfb
Pratiksha Bhosale <pratikshabhosale010@gmail.com>
2024-08-30 14:16:06 +0000

    conversion opreter done

---
+++ javascript/conversionopreter.js
@@ -0,0 +1,9 @@
+let scrore = "33abc"
+
+console.log(typeof scrore);
+console.log(typeof(scrore));
+
+let valueInNumbe = Number(scrore)
+console.log(typeof valueInNumbe);
+console.log(valueInNumbe);
+
